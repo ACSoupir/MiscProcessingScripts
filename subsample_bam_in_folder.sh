f33382e29dfe7fdd7e73a4089edb797d953ea721
Alex Soupir <34483643+ACSoupir@users.noreply.github.com>
2020-11-09 15:57:42 -0500

    Update subsample_bam_in_folder.sh
    update fix

--- subsample_bam_in_folder.sh
+++ subsample_bam_in_folder.sh
@@ -1,63 +1,50 @@
-#!/bin/bash
+##!/bin/bash
 
-# setting defaults for the non-required inputs
-threads=1
-reads_wanted=15000000
-delete=n
-seed=333
+echo "Enter number of threads to use:"
+read threads
+echo "Enter number of reads wanted in output file:"
+read reads_wanted
+echo "Delete original files?"
+read delete
 
-# inputs
-while getopts t:r:d:s:m: flag
-do
-	case "${flag}" in
-		t) threads=${OPTARG};;
-		r) reads_wanted=${OPTARG};;
-		d) delete=${OPTARG};;
-		s) seed=${OPTARG};;
-		m) manifest=${OPTARG};;
-	esac
-done
+if [ "$delete" != "${delete#[Yy]}" ]
+then
+	echo -e "You stated you DO WANT to delete the file"
+else
+	echo -e "You stated you DONT WANT to delete the file"
+fi
 
-# colors
-red='tput setaf 1'
-green='tput setaf 2'
-reset='tput sgr0'
+echo -e "Sampling BAM files in current dirctory\nto ${reads_wanted} reads with ${threads} threads...\n"
 
-# echoing back inputs
-echo "Number of threads to use: ${red}${threads}${reset}"
-echo "Number of reads wanted in output file: ${red}${reads_wanted}${reset}"
-echo "Delete original files? ${red}${delete}{reset}"
-
-echo -e "Sampling BAM files in current directory to ${red}${reads_wanted}${reset} reads with ${red}${threads}${reset} threads...\n"
-
-echo -e "Making ${red}'Subsample'${reset} directory"
+echo -e "Making 'Subsample' directory"
 mkdir -p Subsample
 
-FULL_BAMs=*.bam
+FULL_BAMS=*.bam
 
 for file in $FULL_BAMS
 do
 	echo -e "\n\n"
-	
-	echo "Counting lines in ${red}${file}${reset}"
-	echo -e "\tRunning 'samtools view -c -# ${red}${threads}${reset} ${red}${file}${reset}'"
+
+	echo "Counting lines in ${file}..."
+	echo -e "\tRunning 'samtools view -c -@ ${threads} ${file}'"
 	total_reads=$(samtools view -c -@ ${threads} ${file})
-	echo -e "\tFound ${red}${total_reads}${reset} in ${red}${file}${reset}"
-	ratio=$(echo "scale=4;${reads_wanted}/${total-reads}" | bc)
-	echo -e "\tThis is a sample ratio of ${red}0${ratio}${reset} for ${red}${reads_wanted}${reset} reads.\n"
-
-	echo "Sampling ${red}${file}${reset} to ${red}${reads_wanted}${reset} reads with seed '${red}${seed}${reset}'..."
-	echo -e "\tRunning 'samtools view -v -@ ${red}${threads}${reset} -s ${red}333${ratio}${reset} -o Subsample/${red}${reads_wanted}.${file} ${file}${reset}'"
-	samtools view -v -@ ${threads} -s 333${ratio} -o Subsample/${reads_wanted}.${file} ${file}
+	echo -e "\tfound ${total_reads} in ${file}."
+	ratio=$(echo "scale=5;${reads_wanted}/${total_reads}" | bc)
+	echo -e "\tThis is a sample ratio of 0${ratio} for ${reads_wanted} reads.\n"
+	
+	echo "Sampling ${file} to ${reads_wanted} reads with seed '333'..."
+	echo -e "\tRunning 'samtools view -b -@ ${threads} -s 333${ratio} -o Subsample/${reads_wanted}.${file} ${file}'"
+	samtools view -@ ${threads} -bs 333${ratio} -o Subsample/${reads_wanted}.${file} ${file}
 	sampled_reads=$(samtools view -c -@ ${threads} Subsample/${reads_wanted}.${file})
-	echo -e "\tFinished sampling ${red}${file}${reset} to ${red}${sampled_reads}${reset}."
-	echo -e "\tNew file has ${red}${sampled_reads}${reset}\n"
-
+	echo -e "\tFinished sampling ${file} to ${sampled_reads}."
+	echo -e "\tNew file has ${sampled_reads}\n"
+	
 	if [ "$delete" != "${delete#[Yy]}" ]
 	then
-		echo -e "Deleting ${red}${file}${reset}"
-		rm ${file}
+		echo -e "Deleting ${file}..."
+		echo -e "rm is commented to prevented deleting of files"
+		# rm ${file}
 	else
-		echo -e "You stated you ${red}DON'T WANT${reset} to delete the files"
+		echo -e "You stated you DONT WANT to delete the file"
 	fi
 done
