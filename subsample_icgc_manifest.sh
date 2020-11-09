57a8b4f2579184e87382b5a20aa17bcca292069a
Alex Soupir <34483643+ACSoupir@users.noreply.github.com>
2020-11-09 15:52:53 -0500

    Update subsample_icgc_manifest.sh
    update

--- subsample_icgc_manifest.sh
+++ subsample_icgc_manifest.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source ~/.bash_profile
 
-# setting defaults for the non-required inputs
 threads=1
 reads_wanted=15000000
 delete=n
@@ -12,51 +11,48 @@ while getopts t:r:d:s:m: flag
 do
 	case "${flag}" in
 		t) threads=${OPTARG};;
-		r) reads_wanted=${OPTARG};;
+		r) reads_wanted =${OPTARG};;
 		d) delete=${OPTARG};;
 		s) seed=${OPTARG};;
 		m) manifest=${OPTARG};;
 	esac
 done
 
-# colors
-red='tput setaf 1'
-green='tput setaf 2'
-reset='tput sgr0'
+# colors 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 # echoing back inputs
 echo "Number of threads to use: ${red}${threads}${reset}"
 echo "Number of reads wanted in output file: ${red}${reads_wanted}${reset}"
-echo "Delete original files? ${red}${delete}{reset}"
-echo "Manifest file to use: ${red}${manifest}{reset}"
+echo "Delete original files? ${red}${delete}${reset}"
+echo "Manifest file to use: ${red}${manifest}${reset}"
 
-echo -e "Sampling BAM files in current directory to ${red}${reads_wanted}${reset} reads with ${red}${threads}${reset} threads...\n"
+echo -e "Sampling BAM files in current dirctory to ${red}${reads_wanted}${reset} reads with ${red}${threads}${reset} threads...\n"
 
 echo -e "Making ${red}'Subsample'${reset} directory"
 mkdir -p Subsample
 echo -e "Making temporary directory"
 mkdir -p temp
-echo -e "Making ${red}clippedReads${reset} folder"
+echo -e "Making ${red}clippedReads${reset} foler"
 mkdir -p clippedReads
 
-# extract the object-ids out of the ICGC manifest file - column 3
 cut -f3 ${manifest} > ids.txt
 tail -n +2 ids.txt > ids.tmp
 mv ids.tmp ids.txt
 
-# cut out the 3rd column adn remove the header named 'object_id'
-ids = $(cut -f3 ${manifest})
+ids=$(cut -f3 ${manifest})
 remove=object_id
 ids_clean=${ids[@]/$remove}
 
-# running score-client download for list of object-ids
 for object_id in $ids_clean
 do
 	echo -e "Downloading ICGC object ID ${red}${object_id}${reset}..."
 	score-client --profile collab download --object-id ${object_id} --output-dir temp
 	echo -e "Download of ${red}${object_id}${reset} complete"
 	
-	echo -e "Beginning BAM Processing and Sampling..."
+	echo -e "Beginning BAM Processing and Sampling.."
 	FULL_BAMS=$(ls temp/*.bam | xargs -n 1 basename)
 	for bam in $FULL_BAMS
 	do
@@ -65,21 +61,37 @@ do
 		echo -e "\tCreating BAM index file for ${red}${bam}${reset}..."
 		samtools index -b -@ ${threads} temp/${bam}
 		echo -e "\t${red}${bam}${reset} index created\n"
-		
+
 		echo -e "\tCounting records.."
 		total_reads=$(samtools view -c -@ ${threads} temp/${bam})
 		echo -e "\tfound ${red}${total_reads}${reset} in ${red}${bam}${reset}."
 		ratio=$(echo "scale=5;${reads_wanted}/${total_reads}" | bc)
-		echo -e "\tThis is a samplt ratio of ${red}0${ratio}${reset} for ${red}${reads_wanted}${reset} reads.\n"
+		echo -e "\tThis is a sample ratio of ${red}0${ratio}${reset} for ${red}${reads_wanted}${reset} reads.\n"
+		
+		echo -e "\tSampling ${red}${bam}${reset} to ${red}${reads_wanted}${reset} reads with seed '${red}333${reset}'..."
+		samtools view -@ ${threads} -bs 333${ratio} -o Subsample/${reads_wanted}.${bam} temp/${bam}
+		sampled_reads=$(samtools view -c -@ ${threads} Subsample/${reads_wanted}.${bam})
+		echo -e "\tFinished sampling ${red}${bam}${reset} to ${red}${reads_wanted}${reset}."
+		echo -e "\t${red}${reads_wanted}.${bam}${reset} file has ${red}${sampled_reads}${reset}\n"
 		
 		if [ "$delete" != "${delete#[Yy]}" ]
 		then
 			echo -e "Deleting ${red}${bam}${reset}..."
 			rm temp/${bam}
 		else
-			echo -e "${red}You stated you DON'T WANT to delete the full BAM files${reset}."
+			echo -e "${red}You state you DON'T WANT to delete the full BAM files${reset}"
 		fi
-	done
+	done	
 done
 
 rm ids.txt
+
+: '
+#only get the mapped reads from bam
+samtools view -b -F 4 ${input_bam} > ${output_bam}
+samtools view -f 0x40 ${output_bam} > ${read1.bam}
+samtools view -f 0x80 ${output_bam} > ${read2.bam}
+
+#getting the softclipped reads
+extractSoftclippedRetain 
+'
